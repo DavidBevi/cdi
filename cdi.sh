c2f6ae345918486e4a558fd4bd2d4103a2135bce
DavidBevi <61429850+DavidBevi@users.noreply.github.com>
2026-08-02 01:55:34 +0200

    v2.1.1 (🩵) - Medium refactoring, comments, cleanup
    I did some refactoring, the code that fetches the selected subdir, builds pages, and prints the subdirs-list with the highlight is now executed after a single check (if subdirs-len>0) instead of multiple blocks with the same check repeated.
    
    I continued cleaning up and documenting the code in order to help newcomers.

--- cdi.sh
+++ cdi.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 #
 # CDI - Change Dir Interactive - https://github.com/DavidBevi/cdi
-# v2.1 (2026-07-28)
+# v2.1.1 (2026-08-01)
 #
 # SYNOPSIS
-# CDI displays the Working Dir and its subdirs, and lets the user CD by using
-# the arrow keys. "R" CDs to starting dir, "H" displays help, other keys exit.
+# CDI prints the path of the working dir and the list of its subdirs.
+# It lets the user CD (with arrow keys), reset the starting dir (R),
+# show help (H), and exit (any other key).
 #
 # START
-# The first step is checking if CDI is in Bash and it's sourced, if not it
-# exits with a brief explanation of error and fix.
-# If it passes the checks CDI enters alt mode and hides the cursor.
+# The first step is checking if CDI is in Bash and it's sourced.
+# - If not CDI prints a brief explanation of error and fix and exits.
+# - If it passes the checks CDI enters alt-mode and hides the cursor.
 #
-# HELP MODE
-# Because of its nature CDI doesn't need any argument, therefore if arg(s) are
-# given CDI will set MODE=HELP and the help page will be displayed. Pressing
-# any key sets MODE=NORMAL and resumes the normal execution.
+# HELP STATE
+# Because of its nature CDI doesn't need any argument, therefore if they are
+# given CDI will set STATE=HELP and the help page will be displayed.
+# Pressing any key sets STATE=NORMAL and resumes the normal execution.
 #
 # MAIN LOOP
-# - If MODE=HELP -> display help screen
+# - If STATE==HELP -> display help screen
 # - Clear screen
 # - Print title, subtitle, $PWD
 # - Check if subdirs-list is cached, (re)build cache when needed
-# - Check and validate selection / subdir to be highlighted
-# - Check $ROWS and paginize subdirs-list when it doesn't fit the window
-# - Print subdirs-list or the proper page
-# - Print highlight by overwriting selected subdir in inverted style
-# - Wait for user input -> can change MODE, can CD
+# - If ((subdirs-list < 1))
+#   -> then print "no subdirs"
+#   -> else build and print subdirs-list with highlight
+# - Wait for user input -> can change STATE, can CD
 #
 # END
-# When the MODE is neither NORMAL nor HELP the main loop breaks and CDI exits.
-# First it exits alt mode and restores the cursor visibility.
-# Then if MODE==EXIT it will exit 0, else it will print the error and exit 3.
+# When the STATE is neither NORMAL nor HELP the main loop breaks and CDI exits.
+# First it exits alt-mode and restores the cursor visibility.
+# Then if STATE==EXIT it will exit 0, else it will print the error and exit 3.
 #
 # LICENSE
 # - This free/libre software is provided "AS IS", with NO WARRANTY.
@@ -39,36 +39,37 @@
 # - Derivatives must share their source code and this same license.
 #
 
-# Workaround to print an helpful msg in some shells that crash (Fish)
-_=" this script must be used with Bash shell
-#   (You're using a different shell, it crashed)
-#   WIKI: https://simple.wikipedia.org/wiki/Bash
-" #_____________________________________________
-unset _
-
 # Abort if not in Bash (compacted POSIX syntax)
 [ -z "$BASH_VERSION" ] && { printf "ERROR: this script must be used with Bash.
 WIKI: https://simple.wikipedia.org/wiki/Bash \nEXITING\n" >&2; return 1; }
+
+# Script is not 100% compatible with Fish, so Fish won't run it, but since
+# Fish echoes the first invalid statement we state an helpful msg
+_=" ⚠️ This script must be used with Bash shell
+#   (You're using a different incompatible shell)
+#   WIKI: https://simple.wikipedia.org/wiki/Bash
+" #_____________________________________________
+unset _
 
 # Abort if not sourced (= "source cdi" or ". cdi")
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     basename="${0##*/}"
     red="\e[1;4:4;31m"
     green="\e[1;4:4;32m"
-    bad="'${red}bash $basename\e[0m' or '${red}./$basename\e[0m'"
-    good="'${green}. $basename\e[0m'"
-    echo -e "ERROR: this script doesn't work with $bad.\nFIX: use an" \
-        "interactive Bash session, and launch with $good. \nEXITING" >&2
+    bad_cmd="'${red}bash $basename\e[0m' or '${red}./$basename\e[0m'"
+    good_cmd="'${green}. $basename\e[0m'"
+    echo -e "ERROR: this script doesn't work with $bad_cmd.\nFIX: use an" \
+        "interactive Bash session, and launch with $good_cmd. \nEXITING" >&2
     exit 2
 fi
 
 # GLOBALS #####################################################################
 STARTING_DIR="$PWD"
-# when [ number of arguments < 0 ] -> MODE=HELP
-MODE="NORMAL"; (($#>0)) && MODE="HELP"
+# when [ number of arguments > 0 ] -> STATE=HELP
+STATE="NORMAL"; (($# > 0)) && STATE="HELP"
 # associative arrays (maps)
 declare -A HIGHLIGHTED_ITEM_INDEX HIGHLIGHTED_ITEM_NAME
-# other vars (I don't want many rows for these)
+# other vars
 declare CACHED_DIR_MTIME SUBDLIST SUBDLEN PAGELEN PAGES PAGE
 
 # FUNCTIONS ###################################################################
@@ -93,11 +94,11 @@ show_help() {
     # wait for input, then consume the input buffer to avoid issues
     read -rsn1
     while IFS= read -rsn1 -t 0.01; do :; done
-    # set mode back to "NORMAL" so next cycle resumes CDI
-    MODE="NORMAL"
+    # (re)set STATE=NORMAL to avoid printing help-screen again
+    STATE="NORMAL"
 }
 
-# Display the "normal" header and current directory
+# Display title, subtitle, and working directory
 print_header_and_working_dir() {
     # print the title (bold+inverted style)
     echo -e "\e[1;7m CDI: Change Dir Interactive - by DavidBevi \e[0m"
@@ -112,54 +113,43 @@ build_subdirs() {
     # build a string with PWD and CACHED_DIR_MTIME (modification time)
     local curr_dir_mtime
     curr_dir_mtime="$PWD::$(stat -c %Y .)"
-    #
     # return if the cached and the current dir_mtime strings are equal
     if [[ "$CACHED_DIR_MTIME" == "$curr_dir_mtime" ]]; then return; fi
-    #
-    # else save dir_mtime to cache and rebuild the subdirs list
+    # else save dir_mtime to cache and rebuild the subdirs-list
     CACHED_DIR_MTIME="$curr_dir_mtime"
     SUBDLIST=(*/)
+    # if no subdirs then SUBDLIST will contain only "*/" -> clean up
+    if [[ ${SUBDLIST[0]} == '*/' ]]; then SUBDLIST=(); fi
     SUBDLEN="${#SUBDLIST[@]}"
 }
 
-# Fetch the selected dir that will be highlighted
-build_highlight() {
-    # return if curr-dir has no subdirs
-    if [[ $SUBDLEN == 0 ]]; then return; fi
+# Fetch the terminal window's height and paginize subdirs-list if needed
+build_highlight_and_page() {
+    local _ starting_row
     # use modulo to ensure that the index is valid
     HIGHLIGHTED_ITEM_INDEX["$PWD"]=$((
         (HIGHLIGHTED_ITEM_INDEX[$PWD] + SUBDLEN) % SUBDLEN ))
     # save the name of the highlighted item
-    HIGHLIGHTED_ITEM_NAME["$PWD"]="${SUBDLIST[${HIGHLIGHTED_ITEM_INDEX[$PWD]}]}"
-}
-
-# Fetch the terminal window's height and paginize subdirs-list if needed
-build_pages() {
-    local _ row
+    HIGHLIGHTED_ITEM_NAME["$PWD"]="${SUBDLIST[
+        ${HIGHLIGHTED_ITEM_INDEX[$PWD]}]}"
     # ask the cursor pos, keep only the row
-    IFS='[;' read -p $'\e[6n' -d R -rs _ row _ _
-    # ensure row is numeric, else script crashes
-    [[ "$row" =~ ^[0-9]+$ ]] || row=4
+    IFS='[;' read -p $'\e[6n' -d R -rs _ starting_row _ _
+    # ensure starting_row is numeric, else script crashes
+    [[ "$starting_row" =~ ^[0-9]+$ ]] || starting_row=4
     # update globals
-    PAGELEN=$((LINES - row - 2))
+    PAGELEN=$((LINES - starting_row - 2))
     PAGES=$(( (SUBDLEN + PAGELEN - 1) / PAGELEN ))  # ceil
     PAGE=$(( ${HIGHLIGHTED_ITEM_INDEX["$PWD"]} / PAGELEN ))  # floor
 }
 
 # Display subdirs list, highlight the current selection
-print_page_of_subdirs_with_highlight() {
-    # return if no list
-    if [[ "${SUBDLIST[0]}" == "*/" ]]; then
-        echo -e "\e[33m └─ [no dirs]\e[0m"
-        return
-    fi
-    #
+print_page_with_highlight() {
     # when multiple pages: write "Page X of Y"
     if ((PAGES > 1)); then
         echo -e " │  \e[33mPage $((PAGE+1)) of $PAGES\e[0m"
     fi
     #
-    # display curr page using array expansion with slicing
+    # print curr page using array expansion with slicing
     echo -en "\e[s"  # SAVE cursor pos
     printf ' ├─ %s\n' "${SUBDLIST[@]:$((PAGE*PAGELEN)):$PAGELEN}"
     echo -en "\e[A └"  # replace ├ in last row with └
@@ -210,29 +200,30 @@ left() {
     if [[ "$PWD" == "/" ]]; then return; fi
     # save child_dir and CD up
     local child_dir="${PWD##*/}/"
-    cd .. || MODE="ERROR DOING CD UP"
+    cd .. || STATE="ERROR DOING CD UP"
     # find and highlight child_dir
     local i=0
-    for d in */; do
-        if [[ "$d" == "$child_dir" ]]
-            then break
-            else ((i++))
+    for subdir in */; do
+        if [[ "$subdir" != "$child_dir" ]]; then
+            ((i++))
+        else
+            HIGHLIGHTED_ITEM_INDEX["$PWD"]="$i"
+            HIGHLIGHTED_ITEM_NAME["$PWD"]="$child_dir"
+            break
         fi
     done
-    HIGHLIGHTED_ITEM_NAME["$PWD"]="$child_dir"
-    HIGHLIGHTED_ITEM_INDEX["$PWD"]=$i
 }
 
 help() {
-    MODE="HELP"
+    STATE="HELP"
 }
 
 restore() {
-    cd "$STARTING_DIR" || MODE="ERROR RESTORING STARTING DIR"
+    cd "$STARTING_DIR" || STATE="ERROR RESTORING STARTING DIR"
 }
 
 set_exit() {
-    MODE="EXIT"
+    STATE="EXIT"
 }
 
 # BODY ########################################################################
@@ -241,24 +232,27 @@ set_exit() {
 tput smcup civis
 
 # Main loop
-while [[ $MODE == "HELP" ]] || [[ $MODE == "NORMAL" ]]; do
-    if [[ $MODE == "HELP" ]]; then show_help; fi
+while [[ $STATE == "HELP" ]] || [[ $STATE == "NORMAL" ]]; do
+    if [[ $STATE == "HELP" ]]; then show_help; fi
     clear
     print_header_and_working_dir
     build_subdirs
-    build_highlight
-    build_pages
-    print_page_of_subdirs_with_highlight
-    wait_for_input  # can update MODE + HIGHLIGHTED_ITEM
+    if (($SUBDLEN < 1)); then
+        echo -e "\e[33m └─ [no dirs]\e[0m"
+    else
+        build_highlight_and_page
+        print_page_with_highlight
+    fi
+    wait_for_input  # can update STATE + HIGHLIGHTED_ITEM
 done
 
 # Exit alt-mode, show cursor
 tput cnorm rmcup
 
 # Exit script
-if [[ "$MODE" == "EXIT" ]]; then
+if [[ "$STATE" == "EXIT" ]]; then
     return 0
 else
-    echo "$MODE -- cdi aborted" >&2
+    echo "$STATE -- cdi aborted" >&2
     return 3
 fi
